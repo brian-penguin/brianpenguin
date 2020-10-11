88a265bf7601bfadb4d939775cb979f1a7a52e30
Brian Tenggren <btenggren20@gmail.com>
2020-10-11 17:04:35 -0400

    Lein new!

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to brian-penguin
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/brian_penguin/core_test.clj
@@ -0,0 +1,7 @@
+(ns brian-penguin.core-test
+  (:require [clojure.test :refer :all]
+            [brian-penguin.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
